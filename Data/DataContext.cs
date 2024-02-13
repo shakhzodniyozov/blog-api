9d391a48996839700b979175f93f64ca672ff280
Shakhzod Niyozov <shakhzod.n30@gmail.com>
2024-02-13 15:29:04 +0500

    Add DbContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace blog_api;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfig());
+        modelBuilder.ApplyConfiguration(new PostConfig());
+    }
+}
