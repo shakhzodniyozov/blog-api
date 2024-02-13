3962b58650a068a45fe017a8c05bc6fa65c3f6fa
Shakhzod Niyozov <shakhzod.n30@gmail.com>
2024-02-13 14:33:39 +0500

    Add Models

---
+++ Models/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace blog_api;
+
+public class Post
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Body { get; set; }
+}
